2aa367686437b79232adc87fbb82a5b772f5a7c5
Alexandre <nunesalamino@gmail.com>
2025-04-24 20:36:08 -0300

    LDJ-7 Criação das Classes Principais
    Criação das classes Jogo, Cliente e Aluguel.

---
+++ vo/Jogo.java
@@ -0,0 +1,40 @@
+package vo;
+
+public class Jogo {
+    private String nome;
+    private String plataforma;
+    private String genero;
+    private boolean disponibilidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+}
